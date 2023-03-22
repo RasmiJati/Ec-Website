fe8a323fcdf5695544743c2a49544f5df4dee943
RasmiJati <jatirasmi65@gmail.com>
2023-03-22 05:52:58 +0100

    performed edit operation for user

--- src/main/java/com/rasmijati/controller/UserController.java
+++ src/main/java/com/rasmijati/controller/UserController.java
@@ -22,7 +22,8 @@ public class UserController {
         userRepository = new UserRepository();
         create();
         findAll();
-        delete();
+        edit();
+//        delete();
     }
 
 //static is used as only static variable and method can be used in other static method i.e main method    
@@ -72,7 +73,47 @@ public class UserController {
         System.out.println("Enter Id for delete operation : ");
         Scanner sc = new Scanner(System.in);
         Long id = sc.nextLong();
-        User user = new UserRepository().findById(id);
+        User user = userRepository.findById(id);
         userRepository.delete(user);
+    }
+
+    public static void edit() {
+        Long id = null;
+        String name = null;
+        String address = null;
+        String phone = null;
+        String email = null;
+        String password = null;
+        UserType role = null;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Id for edit operation : ");
+        id = sc.nextLong();
+        User user = userRepository.findById(id);
+
+        if (user == null) {
+            System.out.println("User of id " + id + "not found");
+        } else {
+
+            System.out.println("Enter name : ");
+            name = sc.next();
+
+            System.out.println("Enter address : ");
+            address = sc.next();
+
+            System.out.println("Enter phone number : ");
+            phone = sc.next();
+
+            System.out.println("Enter email : ");
+            email = sc.next();
+
+            System.out.println("Enter password : ");
+            password = sc.next();
+
+            System.out.println("Enter user type (ADMIN / USER) : ");
+            String entered_role = sc.next().toUpperCase();
+            role = UserType.valueOf(entered_role);
+        }
+        user = new User(id, name, address, phone, email, password, role);
+        userRepository.edit(user);
     }
 }
